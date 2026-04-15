643ae423a56ce8b0c5e1b3c615e16b15b413e081
vinay2003 <vinaysharma31681@gmail.com>
2026-04-15 20:24:20 +0530

    fix: resolve node runtime module not found error by pointing types package to dist and using esm extensions

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -7,7 +7,7 @@
  *   - Enums
  */
 
-export * from './user';
-export * from './vendor';
-export * from './booking';
-export * from './auth';
+export * from './user.js';
+export * from './vendor.js';
+export * from './booking.js';
+export * from './auth.js';
